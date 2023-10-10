a76cdd3fc8af1f18bc8964d6f043a298a6dc74f2
Oke Precious Abioye <okepreciousab@gmail.com>
2023-10-10 15:22:51 +0100

    i added some functions

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-// let SpeechRecognition = SpeechRecognition;
+
 let detectSpeech=()=>{
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
     textContainer.innerHTML="Wetin ypu dey talk";
     textbox.style.display = "flex";
+
+    recognition.onstart = function(){
+        action.innerHTML = "listening, please speak...";
+    }
+    recognition.onspeeched = function(){
+        action.innerHTML = "stopped listening, hope you are done...";
+        recognition.stop();
+    }
+    recognition.onresult = function(event){
+        var transcript = event.results[0][0].transcript;
+        var confidence = event.results[0][0].confidence;
+    }
+    recognition.start()
+
 }
